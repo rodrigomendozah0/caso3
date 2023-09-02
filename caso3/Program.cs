dad133aa556b45628c67bd471f2164af2759d325
Rodrigo <Rodrigo@HP_RODRIGO>
2023-09-02 01:29:59 -0500

    Agregar archivos de proyecto.

---
+++ caso3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caso3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alumno;
+            float pagoAlumno, Total;
+            Console.WriteLine("Cuantos alumnos iran de viaje: ");
+            alumno = int.Parse (Console.ReadLine());    
+             if (alumno >= 100)
+            {
+                pagoAlumno = 65;
+            }
+             else
+            {
+                if (alumno >= 50)
+                {
+                    pagoAlumno = 70;
+                }
+                else
+                {
+                    if (alumno >= 30)
+                    {
+                        pagoAlumno = 95;
+                    }
+                    else
+                    {
+                        pagoAlumno = 4000;
+                    }
+                }
+            }
+            Total = pagoAlumno * alumno;
+            Console.WriteLine("Pago individual es de: S/" + pagoAlumno);
+            Console.WriteLine("El pago total es de: S/" + Total);
+            Console.ReadKey();
+        }
+    }
+}
